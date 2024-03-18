917fe75a7a8d3d6496d37fc4b0d79ba731171e01
Jenelle Holloway <jholloway@griddynamics.com>
2024-03-18 14:02:27 -0500

    initialize database

---
+++ database.js
@@ -0,0 +1,36 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const DBSOURCE = "db.sqlite";
+const createUser =
+  "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL)";
+const createExercise =
+  "CREATE TABLE exercises (exercise_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, duration INTEGER NOT NULL, description TEXT NOT NULL, date TEXT DEFAULT DATE('now'))";
+
+const insertUser = "INSERT INTO users (username) VALUES (?)";
+const insertExercise =
+  "INSERT INTO exercises (user_id, duration, description, date) VALUES (?,?,?,?)";
+
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQLite database");
+    db.run(createUser, (err) => {
+      if (err) {
+        //console.error(err);
+      } else {
+        db.run(insertUser, ["testUser"]);
+      }
+    });
+    db.run(createExercise, (err) => {
+      if (err) {
+        //console.log(error);
+      } else {
+        db.run(insertExercise, [1, 60, "Burpees", "2024-03-18"]);
+      }
+    });
+  }
+});
+
+module.exports = db;
